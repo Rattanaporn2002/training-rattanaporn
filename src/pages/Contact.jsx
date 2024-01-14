23c09959931272226fb94bd516f1fa6fcf7799df
Rattanaporn2002 <rattanaporn1812.n@gmail.com>
2024-01-14 14:08:16 +0700

    commit-message

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+        <h1>ติดต่อ</h1><hr />
+        <h2>NVC Training Center</h2>
+        <h4>Address: 90 Tesa Rd., Mueang, Nakhon Pathom 73000 </h4>
+        <h4>Tel: 034-252790 </h4>
+        <h4>Email: admin@nvctrainingcenter.com </h4>
+    </div>
+  ) ;
+}
+
+export default Contact
---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <h1>หน้าหลัก</h1><hr />
+      <center>
+      <img src="f1a1508f7568bd52b78c88011e73a4bb.jpg" alt="" />
+      </center>
+      </div>
+      
+
+  )
+}
+
+export default Home
